63ab9c9df5f14944908b9918e47f633374d5523d
kostanzhoglo <jeffreywithers@gmail.com>
2018-06-19 11:35:29 -0400

    Complete with time for the code challenge

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,9 @@
-class Image {
-  
-
-}
+// class Image {
+//   constructor(id, url, name, like_count) {
+//     this.id = id;
+//     this.url = url;
+//     this.name = name;
+//     this.like_count = like_count;
+//   }
+//
+// }
